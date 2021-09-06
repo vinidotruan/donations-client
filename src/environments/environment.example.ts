2ba7a4c3ef58d3b6028e5a7ec8eb023556ad66be
Ruan Vinícius <contatoruanvinicius@gmail.com>
2021-09-06 02:16:11 -0300

    - Remove .env

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'dasdasdasd',
+    authDomain: 'dasdasdasd',
+    projectId: 'dasdasdasd',
+    storageBucket: 'dasdasdasd',
+    messagingSenderId: 'dasdasdasd',
+    appId: 'dasdasdasd',
+    measurementId: 'dasdasdasd',
+  },
+};
